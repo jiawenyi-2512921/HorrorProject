6b4a22d20ee2e630ec6160904abd69e363122657
jiawenyi-2512921 <2512921@mail.nankai.edu.cn>
2026-04-26 20:06:26 +0800

    核心框架搭建

--- HorrorProject/HorrorProject/Source/HorrorProject/HorrorProject.Build.cs
+++ HorrorProject/HorrorProject/Source/HorrorProject/HorrorProject.Build.cs
@@ -19,27 +19,13 @@ public class HorrorProject : ModuleRules
 			"StateTreeModule",
 			"GameplayStateTreeModule",
 			"UMG",
-			"Slate"
+			"SlateCore"
 		});
 
 		PrivateDependencyModuleNames.AddRange(new string[] { });
 
-		PublicIncludePaths.AddRange(new string[] {
-			"HorrorProject",
-			"HorrorProject/Game",
-			"HorrorProject/Interaction",
-			"HorrorProject/Player",
-			"HorrorProject/Player/Components",
-			"HorrorProject/Variant_Horror",
-			"HorrorProject/Variant_Horror/UI"
+		PrivateIncludePaths.AddRange(new string[] {
+			"HorrorProject"
 		});
-
-		// Uncomment if you are using Slate UI
-		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-
-		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
-
-		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
 	}
 }
